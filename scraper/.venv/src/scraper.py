cc537babea5fa9addacfd6673087b037b3ba27af
srzvan <sbingu.razvan@gmail.com>
2022-04-10 22:21:11 +0300

    WIP scraper - select the <ol/> that contains the HTML elements

--- scraper/.venv/src/scraper.py
+++ scraper/.venv/src/scraper.py
@@ -1,32 +1,31 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-# data items
-# {
-#   name: string
-#   deprecated:boolean
-#   non-standard: boolean
-#   experimental: boolean
-# }
 
-# selector
-# '.sidebar-inner > div > ol > li:nth-child(7) ol'
-data = []
+def scraper():
+    """
+      Scrape https://developer.mozilla.org/en-US/docs/Web/HTML/Element
+      for the list of all HTML elements and the categories they're a part
+      of (non-standard, deprecated, experimental)
+    """
+    data = []
 
-with urllib.request.urlopen('https://developer.mozilla.org/en-US/docs/Web/HTML/Element') as response:
-   html = response.read()
-   soup = BeautifulSoup(html, 'html.parser')
+    with urllib.request.urlopen('https://developer.mozilla.org/en-US/docs/Web/HTML/Element') as response:
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
 
-   sidebar_heading = soup.find("h4", class_='sidebar-heading')
-   sidebar_ol = sidebar_heading.find_next_sibling('div').ol
+    sidebar_heading = soup.find("h4", class_='sidebar-heading')
+    sidebar_ol = sidebar_heading.find_next_sibling('div').ol
 
-  #  print(sidebar_ol)
+    nested_list_item = None
+    for index, list_item in enumerate(sidebar_ol):
+        if index == 13:
+            nested_list_item = list_item
+            break
 
-   html_elements_list = None
-   for index, list_item in enumerate(sidebar_ol):
-      if index == 6:
-        print(list_item)
-        html_elements_list = list_item
-        break
+    html_elements_list = nested_list_item.details.ol
+    for index, list_item in enumerate(html_elements_list):
+        print(index, list_item)
 
-  #  print(html_elements_list)
+
+scraper()
